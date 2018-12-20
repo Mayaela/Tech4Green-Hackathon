a4e53ec25b9ced8d08f65147fd5c09b0d91165bc
DavidPayet <davidpayet570@gmail.com>
2018-12-20 15:36:56 +0100

    create react app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Route onUpdate={window.scrollTo(0, 0)} exact path="/" component={HomePage} />
+        <Route onUpdate={window.scrollTo(0, 0)} path="/alimentation" component={PageAlimentation} />
+        <Route onUpdate={window.scrollTo(0, 0)} path="/mode" component={PageMode} />
+        <Route onUpdate={window.scrollTo(0, 0)} path="/sante_beaute" component={PageSanteBeaute} />
+        <Route onUpdate={window.scrollTo(0, 0)} path="/maison_ecolo" component={PageMaisonEcolo} />
+        <Route onUpdate={window.scrollTo(0, 0)} path="/transports" component={PageTransports} />
+        <Route onUpdate={window.scrollTo(0, 0)} path="/technologie" component={PageTechnologie} />
+        <Route onUpdate={window.scrollTo(0, 0)} path="/don_troc" component={PageDonTroc} />
+        <Route onUpdate={window.scrollTo(0, 0)} path="/occasions" component={PageOccasions} />
+        <Route onUpdate={window.scrollTo(0, 0)} path="/reparation" component={PageReparation} />
+        <Route onUpdate={window.scrollTo(0, 0)} path="/sengager" component={PageSengager} />
+    </div>
+    );
+  }
+}
+
+export default App;

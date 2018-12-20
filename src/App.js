60a19ff1ec0ed9112ff85ac2adc6bd9ab355efe6
Lingling Tabuteau <lingling.tabuteau@gmail.com>
2018-12-20 18:20:38 +0100

    first version barre de recherch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import SearchBar from './components-pages/SearchBar';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route onUpdate={window.scrollTo(0, 0)} exact path="/" component={HomePage} />
-        <Route onUpdate={window.scrollTo(0, 0)} path="/alimentation" component={PageAlimentation} />
-        <Route onUpdate={window.scrollTo(0, 0)} path="/mode" component={PageMode} />
-        <Route onUpdate={window.scrollTo(0, 0)} path="/sante_beaute" component={PageSanteBeaute} />
-        <Route onUpdate={window.scrollTo(0, 0)} path="/maison_ecolo" component={PageMaisonEcolo} />
-        <Route onUpdate={window.scrollTo(0, 0)} path="/transports" component={PageTransports} />
-        <Route onUpdate={window.scrollTo(0, 0)} path="/technologie" component={PageTechnologie} />
-        <Route onUpdate={window.scrollTo(0, 0)} path="/don_troc" component={PageDonTroc} />
-        <Route onUpdate={window.scrollTo(0, 0)} path="/occasions" component={PageOccasions} />
-        <Route onUpdate={window.scrollTo(0, 0)} path="/reparation" component={PageReparation} />
-        <Route onUpdate={window.scrollTo(0, 0)} path="/sengager" component={PageSengager} />
-    </div>
+        <Switch>
+          {/* <Route onUpdate={window.scrollTo(0, 0)} exact path="/" component={HomePage} /> */}
+          {/* <Route onUpdate={window.scrollTo(0, 0)} path="/alimentation" component={PageAlimentation} />
+          <Route onUpdate={window.scrollTo(0, 0)} path="/mode" component={PageMode} />
+          <Route onUpdate={window.scrollTo(0, 0)} path="/sante_beaute" component={PageSanteBeaute} />
+          <Route onUpdate={window.scrollTo(0, 0)} path="/maison_ecolo" component={PageMaisonEcolo} />
+          <Route onUpdate={window.scrollTo(0, 0)} path="/transports" component={PageTransports} />
+          <Route onUpdate={window.scrollTo(0, 0)} path="/technologie" component={PageTechnologie} />
+          <Route onUpdate={window.scrollTo(0, 0)} path="/don_troc" component={PageDonTroc} />
+          <Route onUpdate={window.scrollTo(0, 0)} path="/occasions" component={PageOccasions} />
+          <Route onUpdate={window.scrollTo(0, 0)} path="/reparation" component={PageReparation} />
+          <Route onUpdate={window.scrollTo(0, 0)} path="/sengager" component={PageSengager} /> */}
+        </Switch>
+        <SearchBar />
+      </div>
     );
   }
 }
---
+++ src/components-pages/SearchBar.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import Applist from '../applist.json';
+import escapeRegExp from 'escape-string-regexp';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: "",
+    }
+  }
+
+  updateQuery = (query) => {
+    this.setState({
+      query: query.trim()
+    })
+  }
+
+  render() {
+    let showingApplications = [];
+    if(this.state.query){
+      const match = new RegExp(escapeRegExp(this.state.query), 'i')
+      showingApplications = Applist.filter((appli) => match.test(appli.company))
+    } else{
+      showingApplications = Applist;
+    }
+
+    return (
+      <div className="SearchBar">
+        <input
+          type="text"
+          value={this.state.query}
+          onChange={event => this.updateQuery(event.target.value)}
+          placeholder="Search Application"
+        />
+          <button>Search</button>
+        <ol className='application-list'>
+          {showingApplications.map((appli) => (
+            <li key={appli.id} className='application-list-name'>
+              {appli.company}
+            </li>
+          ))
+          }
+        </ol>
+      
+      </div>
+    );
+  }
+}
+
+export default SearchBar;

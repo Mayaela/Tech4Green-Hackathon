f876f88a37228905f47a0a123e76e609888fd4b9
Maya Safadi <36012265+Mayaela@users.noreply.github.com>
2018-12-21 10:57:14 +0100

    Update Application.jsx

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import Applist from '../applist.json';
 
-const Application =()=>{
-  return (
-    <div>
-      <h2>{Applist[1].Company}</h2>
-      <p>{Applist[1].Category}</p>
-      <p>{Applist[1].Description}</p>
-    </div>
-  );
-}
+const PageApplication = () => (
+  <div>
+    <h2>{Applist[1].Company}</h2>
+    <p>{Applist[1].Category}</p>
+    <p>{Applist[1].Description}</p>
+  </div>
+);
 
-export default Application;
+export default PageApplication;
